2520936e4d6aa0194a3a55df2c2ca2f0bc093c52
Bombo <bombozaur6@gmail.com>
2025-07-15 15:23:45 +0200

    Initial commit

---
+++ YAPO/src/test/java/com/example/YAPO/YapoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YAPO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YapoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
