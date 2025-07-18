66a286e1dcb2e79fc56aeba47bac589577e0dcaf
Bombo <bombozaur6@gmail.com>
2025-07-18 15:39:16 +0200

    proper url mapping

--- YAPO/src/main/java/com/example/YAPO/controlers/PlantController.java
+++ YAPO/src/main/java/com/example/YAPO/controlers/PlantController.java
@@ -4,12 +4,12 @@ import com.example.YAPO.models.Plant;
 import com.example.YAPO.repositories.PlantRepo;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "https://localhost:4200/")
+@RequestMapping("/plants")
 public class PlantController {
     private PlantRepo plantRepo;
 
@@ -17,12 +17,12 @@ public class PlantController {
         this.plantRepo = plantRepo;
     }
 
-    @GetMapping("/plants")
+    @GetMapping("/")
     public List<Plant> plantsPage() {
         return plantRepo.findAll();
     }
 
-    @GetMapping("/plants/{id}")
+    @GetMapping("/{id}")
     public Optional<Plant> plantByIdPage(@PathVariable String id) {
         return plantRepo.findById(Integer.valueOf(id));
     }
